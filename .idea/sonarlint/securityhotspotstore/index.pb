afb1b9a68511a4b06163fdd96f658f6b8a728da2
cris-cod <aarevalo.cristian.17@gmail.com>
2024-05-19 21:08:46 -0500

    create first endPoint

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+a
+1src/main/java/co/com/screenplay/project/Main.java,e\e\ee7f0f11b34e59774a68a87a109eb2ec1c79c963
+N
+src/main/java/co/com/Main.java,3\6\36a2c7894ce66ee3b3799f0e208cc4f9e8282862
+P
+ src/main/java/co/com/Basics.java,9\2\9274e1c73d2ce09a3b666ccbf78d52332a790fa6
---
+++ src/main/java/co/com/Basics.java
@@ -0,0 +1,39 @@
+package co.com;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class Basics {
+    public static void main(String[] args) {
+        //validate if add Place API is working as expected
+        //given - all input details
+        // when - Submit the API
+        //then  validaste de responde
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        Response response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").
+                body("{\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": -38.383494,\n" +
+                        "    \"lng\": 33.427362\n" +
+                        "  },\n" +
+                        "  \"accuracy\": 50,\n" +
+                        "  \"name\": \"Vender house\",\n" +
+                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "  \"address\": \"29, side layout, cohen 09\",\n" +
+                        "  \"types\": [\n" +
+                        "    \"shoe park\",\n" +
+                        "    \"shop\"\n" +
+                        "  ],\n" +
+                        "  \"website\": \"http://vender.com\",\n" +
+                        "  \"language\": \"French-IN\"\n" +
+                        "}").when().post("/maps/api/place/update/json").
+                then().log().all().assertThat().statusCode(200).extract().response();
+
+        String responseString = response.toString();
+        System.out.println("respuesta " + responseString);
+
+
+    }
+}
