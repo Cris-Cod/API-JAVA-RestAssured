871b64c2d0dd2496c26491f171e21f274299e54a
Cristian <aarevalo.cristian.17@gmail.com>
2024-05-28 16:37:05 -0500

    add Deserialization

---
+++ src/main/java/co/com/apicontract/oAuthTest.java
@@ -0,0 +1,29 @@
+package co.com.apicontract;
+
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+
+public class oAuthTest {
+    public static void main(String[] args) {
+
+        String response = given().formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .formParams("client_secret","erZOWM9g3UtwNRj340YYaK_W")
+                .formParams("grant_type", "client_credentials")
+                .formParams("scope", "trust")
+                .when().log().all()
+                .post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token")
+                .asString();
+
+        System.out.println(response);
+        JsonPath js = new JsonPath(response);
+        String accessToken = js.getString("access_token");
+
+        String responnse2 = given().log().all()
+                .queryParams("access_token", accessToken)
+                .when().log().all()
+                .get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+
+        System.out.println(responnse2);
+    }
+}
